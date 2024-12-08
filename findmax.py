390f39ef0fd127346deca91bd62791988dec12f9
FlySaurus <sachkovko@gmail.com>
2024-12-08 22:25:18 +0300

    findmax.py:modified & numbers.txt:cleared

--- findmax.py
+++ findmax.py
@@ -1,3 +1,17 @@
-with open ("C:/Users/FlySaurus/Documents/new-BMSTU/2024-2025/practice/homework2/numbers.txt") as file:
-    list = [int (i) for i in file]
-print("Максимальное число:", max(list))
+file = open("C:/Users/FlySaurus/Documents/new-BMSTU/2024-2025/practice/homework2/numbers.txt", "a")
+
+file2 = open("C:/Users/FlySaurus/Documents/new-BMSTU/2024-2025/practice/homework2/numbers.txt", "r")
+filelen = len(file2.readlines())
+
+num = 0
+list = []
+print("Если введёте пробел, программа остановится и выведет максимальное число.")
+while num != "":
+    try:
+        num = int(input("Введите число: "))
+        list.append(num)
+    except ValueError:
+        print(f'Максимальное число: {max(list)}')
+        stroka = ("Max number #" + str(filelen+1) + " - " + str(max(list)) + '\n')
+        file.writelines(stroka)
+        break
